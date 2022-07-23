ee940dae9e62072cba82760c852da086c6a4b3ad
hello391 <mmaaxxmmaaxxaa@gmail.com>
2022-07-23 19:35:52 -0400

    Delete shop.js

--- shop.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const ecoSchema = new Schema({
-    guildID: { type: String },
-    shop: { type: Object }
-});
-
-module.exports = model("economyshop", ecoSchema);
